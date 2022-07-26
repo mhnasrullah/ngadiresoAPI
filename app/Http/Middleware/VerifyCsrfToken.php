5b021dad1788b7869deacec07f56bc67479129a7
Hasan Nasrullah <hnasrul22@gmail.com>
2022-07-26 20:03:07 +0700

    dep

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        "http://localhost:8000/api/*"
+        "http://localhost:8000/api/*",
+        "https://young-anchorage-84624.herokuapp.com/api/*",
     ];
 }
